19012301fa4e04629cff4691989561db4df3d77a
AdlerEnergy <104151385+AdlerEnergy@users.noreply.github.com>
2023-11-18 04:39:05 +0000

    Update

--- javascript/input/input.js
+++ javascript/input/input.js
@@ -1,9 +1,23 @@
+
 let btn = document.querySelector("#myButton");
 let input = document.querySelector("#input1")
+let pick = document.querySelector("#picked")
 
 btn.addEventListener("click", button_clicked);
 
 function button_clicked()
 {
     alert(input.value);
+    pick.textContent = input.value + " " + pick.textContent;
+    input.value = "";
+    
 }
+
+
+/*
+let fname = "Aiden";
+for(let i = 0; i<fname.length; i+= 1)
+{
+    alert(fname[i].toUpperCase);
+}
+*/
